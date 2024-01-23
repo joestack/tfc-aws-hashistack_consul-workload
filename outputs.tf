5b31d9145a005ab901009294738b31ef319bf263
JoeStack <joern@hashicorp.com>
2024-01-23 14:09:19 +0100

    output db_clients

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@ output "web_clients" {
   description = "EC2 public IP"
 }
 
-# output "db_clients" {
-#   value       = aws_instance.consul_client_db[*].public_ip
-#   description = "EC2 public IP"
-# }
+output "db_clients" {
+  value       = aws_instance.consul_client_db[*].public_ip
+  description = "EC2 public IP"
+}
 
 output "consul_token" {
   value = local.consul_init_token
